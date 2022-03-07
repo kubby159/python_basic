8d01a4c912521c419331ed76280e2402cf0dcb60
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-03-07 09:47:04 +0900

    [Python] KeywordedArguments

---
+++ KeywordedArguments.py
@@ -0,0 +1,14 @@
+def plus (a,b) : 
+  return a + b
+
+
+#default value.
+result = plus (b = 20,a = 10)
+print(result)
+
+#formatting
+def say_hello (name,age):
+  return f"Hello {name} you are {age} years old"
+
+hello = say_hello('Jay',"12")
+print(hello)

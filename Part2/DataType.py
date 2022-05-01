7881e5a3e86754febe7d32476c4b802e30cf9aee
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-05-01 20:38:18 +0900

    DataType

---
+++ Part2/DataType.py
@@ -0,0 +1,21 @@
+#DataType.
+
+#String
+
+print("Hello"[0]) #H
+print("Hello"[4]) #o
+print("123"+"456") # 123456
+
+
+#Integer
+
+
+print(123+456) # 579
+
+#Float
+print(3.14156)
+
+#Boolean
+
+print(True)
+print(False)

646dd3d74e0f38860483d47c6a404141b360657a
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-05-09 21:54:16 +0900

    project2

--- Part2/mathOperation.py
+++ Part2/mathOperation.py
@@ -4,3 +4,24 @@
 6/3
 print(2**3)
 print(3*(3 + 3)/3-3)
+
+
+#round
+print(round(8/3, 2))
+
+#round off
+print(8 // 3)
+
+#User scores a point
+score=5;
+score += 1
+print(score)
+score *= 1
+print(score)
+score /= 1
+print(score)
+score -=1
+print(score)
+
+# f-string
+print(f"your is {score} ")

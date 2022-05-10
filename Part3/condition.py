4b8544a46bce89d145be2773fec8e473fbb8c193
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-05-10 21:55:33 +0900

    whether the number is odd or even

--- Part3/condition.py
+++ Part3/condition.py
@@ -5,4 +5,20 @@ if height > 120:
   print("you can ride the rollercoaster!")
 else:
   print("Sorry, you have to grow taller before you can ride.")
-  
+
+
+#test 
+#whether if a number is odd or even.
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if number % 2 == 0:
+	print("this number is even")
+else:
+	print("this number is odd")
+
+
+

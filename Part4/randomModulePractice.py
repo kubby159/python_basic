7edb2c8c18cbedacd1def146653115ffda6ef2ee
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-05-14 11:29:57 +0900

    randomGame

---
+++ Part4/randomModulePractice.py
@@ -0,0 +1,16 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+number = int(random.random()*2)
+
+if number == 1:
+	print('Heads')
+else: 
+	print('Tails')
+
+
+
+
+
+
---
+++ Part4/randomTest.py
@@ -0,0 +1,7 @@
+import random 
+
+random_integer = random.randint(100,200)
+print(random_integer)
+
+random_float = round(random.random()*5)
+print(random_float)

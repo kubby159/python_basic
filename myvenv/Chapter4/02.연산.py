76b90445ae5236e4152aa50e34a3e576012bb4c8
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-05-31 13:22:29 +0900

    operator

---
+++ myvenv/Chapter4/02.연산.py
@@ -0,0 +1,20 @@
+#1. 비교연산
+print("- 비교연산자")
+print(2>3)
+print(15 < 30)
+print(1.5 >= 0)
+print(3 <= 3)
+print("팙팗팗" == "팙팚팗")
+print("11111"!= '11111')
+
+
+#2. 논리연산
+print(" - 논리연산자")
+print(4 < 6 and 10 >= 10)
+print("포기하지말아요" != "포기하지말아요" or "나는 할 수 있다" == '나는 할 수 있다') 
+print(not 5 == 5)
+
+#3. 멤버십연산
+print(" - 멤버십연산자")
+print("a" in "abc") #포함되어있다면 True
+print("d" not in "abc") #포함되어있지 않다면 True
